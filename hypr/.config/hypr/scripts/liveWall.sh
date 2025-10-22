9bf6f6e196d0ddb9298291cef85c94b98081c0b4
TG-Jorj-2005 <tanase.george.1905@gmail.com>
2025-10-22 13:27:09 +0300

    miercuri 22 octombrie 2025, 13:27:09 +0300

---
+++ hypr/.config/hypr/scripts/liveWall.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+MONITOR="eDP-1"
+
+VIDEO="$HOME/DotfilesManj/hypr/.config/hypr/wallpaper/moe.mp4"
+
+sleep 2
+
+# Pornește mpvpaper (separat, fără sunet, în buclă, pe tot ecranul)
+mpvpaper "$MONITOR" "$VIDEO" -- --loop --no-audio --geometry=100%x100%
+
